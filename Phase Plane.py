1f66fbcb81cf50c89e72541454a380e3b619e86c
DavidSharam788 <pmyds20@nottingham.ac.uk>
2025-01-31 13:26:21 +0000

    Create Phase Plane.py

---
+++ Phase Plane.py
@@ -0,0 +1,26 @@
+import scipy.integrate as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.matrix([[0,1],[1,0]])
+P = [1,-1]#one producer one consumer
+n = 2
+gamma = 0.5
+P_k = 0.2
+kappa = 1/P_k
+thetazero = [1,0]
+
+def dw(w , t):
+    return [w[1],
+            2 - gamma * w[1] - 2 * kappa * np.sin(w[0])]
+
+x = np.linspace(-np.pi,np.pi,100)
+y = np.linspace(-np.pi,np.pi,100)
+X, Y = np.meshgrid(x, y)
+Xdot = Y
+Ydot =  2 - gamma * Y - 2 * kappa * np.sin(X)
+plt.streamplot(X,Y,Xdot,Ydot)
+plt.xlabel(r'$\Delta\theta$')
+plt.ylabel(r'$w$')
+plt.legend()
+plt.show()
