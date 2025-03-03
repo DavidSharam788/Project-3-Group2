5560482578fc3d9000b47025ec0f0e1974bfb05b
DavidSharam788 <pmyds20@nottingham.ac.uk>
2025-03-03 13:44:36 +0000

    ud

--- criticalAlpha.py
+++ criticalAlpha.py
@@ -5,29 +5,32 @@ import matplotlib.pyplot as plt
 import systemGenerators as SG
 import cascadingFaliures as CF
 
-n=10
+n=30
 kappa = n
 gamma = 1
-gen = 5
-con = 5
+gen = 15
+con = 15
 pas = 0
 k = 4
 p = 0.1
 G = nx.watts_strogatz_graph(n, k, p) 
 P = SG.randomisePower(gen,con,n)
 S = 0
-stepsize = 1
-tol = 0.1
-acastar = 1.3
+stepsize = 0.5
+tol = (n * 2 - 1)/2
+tol2 = int((n * 2 - 1)/2)
+tol3 = 0.5 - np.abs((tol2 - tol))
+print(tol3)
+acastar = 1
 Slast = 0
-while np.abs(S-0.5) > tol:
+while np.abs(S-0.5) > tol3:
     S = CF.dynamicCascade(acastar,G.copy(),P.copy())
     print(S)
     print(acastar)
     if((Slast > 0.5 and S < 0.5) or (Slast < 0.5 and S > 0.5)):
         stepsize = stepsize/2
     if(S-0.5 > 0):
-        acastar -=  tol * stepsize
+        acastar -= stepsize
     elif(S-0.5 < 0):
-        acastar +=  tol * stepsize
+        acastar += stepsize
     Slast = S
